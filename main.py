243de4c57899847610852348b0f1e8d98be40547
Tenzin <deetetee@gmail.com>
2025-05-07 15:45:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def webhook(request):
 # ─── aiohttp Startup Hook ───────────────────────────────────
 async def on_startup(app: web.Application):
     logger.warning(f"✅ RUNTIME ROUTES_MAP raw string: {os.getenv('ROUTES_MAP')}")
-    logger.info(f"📦 ROUTES_MAP from get_route_map: {get_route_map()}")
+    logger.info(f"📦 ROUTES_MAP from get_routes_map: {get_routes_map()}")
     telegram_app.bot_data["ROUTES_MAP"] = get_routes_map()
     telegram_app.bot_data["ALLOWED_SOURCE_CHAT_IDS"] = ALLOWED_SOURCE_CHAT_IDS
     telegram_app.bot_data["ADMIN_CHAT_ID"] = ADMIN_CHAT_ID
